5f6c670479a0ab2e7aaaf58074b8c9652f85087c
Peter Ayeni <ayenicology@gmail.com>
2019-09-03 13:27:45 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     
+     return programmer_hash[:grace_hopper]
+  
 
 end
 
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -78,9 +83,14 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        }
      }
 
-    
+
+    return programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
@@ -102,6 +112,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
      
 end
 
@@ -121,5 +133,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     adding_to_dennis[:dennis_ritchie][:languages][1] = "Assembly"
+     return programmer_hash
 
 end
