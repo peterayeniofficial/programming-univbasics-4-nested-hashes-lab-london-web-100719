1357e91af1c128630fce28e08753609fb6e8c505
Peter Ayeni <ayenicology@gmail.com>
2019-09-03 13:18:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,7 @@ def adding_to_dennis
         }
      }
      
-     adding_to_dennis[:dennis_ritchie][:languages][1] =
+     adding_to_dennis[:dennis_ritchie][:languages][1] = "Assembly"
+     return programmer_hash
 
 end
