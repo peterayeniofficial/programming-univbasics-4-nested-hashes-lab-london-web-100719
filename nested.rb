635a1346d9b7200c0fba8a383a2ea09bfef01542
Peter Ayeni <ayenicology@gmail.com>
2019-09-03 13:09:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,10 +86,10 @@ def adding_matz
      }
      
      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-     programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
-     programmer_hash[:yukihiro_matsumoto][:languages] = "C"
+     programmer_hash[:yukihiro_matsumoto][:languages] =  ["LISP", "C"]
 
-    puts programmer_hash
+
+    return programmer_hash
 end
 
 def changing_alan
